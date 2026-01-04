f459b9aaadaa64bb3f337bcfd0c2653fb0db5b01
Hothaifa Alawi <Hothaifaalawi8@gmail.com>
2026-01-05 02:42:18 +0300

    Merge branch 'main' of https://github.com/Hothaifaq/al-taam-al-raqi

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -16,6 +16,7 @@ export default function About() {
     'خدمة عملاء ممتازة',
   ];
 
+  
   return (
     <div className="min-h-screen bg-background">
       <Header onCartClick={() => setCartOpen(true)} />
